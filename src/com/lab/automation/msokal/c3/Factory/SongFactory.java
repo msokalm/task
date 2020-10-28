2cfaaed93519fa96b6aeff52fcff4493ceafb39e
msokalm <msokalm@gmail.com>
2020-10-28 20:58:32 +0300

    fix

--- src/com/lab/automation/msokal/c3/Factory/SongFactory.java
+++ src/com/lab/automation/msokal/c3/Factory/SongFactory.java
@@ -4,9 +4,26 @@ import com.lab.automation.msokal.c3.enums.Genre;
 import com.lab.automation.msokal.c3.music.Song;
 
 public class SongFactory {
-    public static Song SongFactory
+    public static Song PopSongFactory
             (int bitrate, double size, double time,
              String songName, int rating, int countOfListen) {
         return new Song(bitrate, size, time, songName, rating, countOfListen, Genre.POP);
     }
+    public static Song PunkSongFactory
+            (int bitrate, double size, double time,
+             String songName, int rating, int countOfListen) {
+        return new Song(bitrate, size, time, songName, rating, countOfListen, Genre.PUNK);
+    }
+    public static Song RockSongFactory
+            (int bitrate, double size, double time,
+             String songName, int rating, int countOfListen) {
+        return new Song(bitrate, size, time, songName, rating, countOfListen, Genre.ROCK);
+    }
+    public static Song OtherSongFactory
+            (int bitrate, double size, double time,
+             String songName, int rating, int countOfListen) {
+        return new Song(bitrate, size, time, songName, rating, countOfListen, Genre.OTHER);
+    }
+
 }
+
